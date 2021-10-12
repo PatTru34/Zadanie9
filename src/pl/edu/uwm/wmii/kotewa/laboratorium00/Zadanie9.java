cc82a06a7afd60a9e170550f4a12f34f9ee81c71
Patryk Truchan <162391@student.uwm.edu.pl>
2021-10-12 10:02:57 +0200

    Initial commit

---
+++ src/pl/edu/uwm/wmii/kotewa/laboratorium00/Zadanie9.java
@@ -0,0 +1,12 @@
+package pl.edu.uwm.wmii.kotewa.laboratorium00;
+
+public class Zadanie9 {
+
+    public static void main(String[] args) {
+        System .out. println ("  ^_____^          -------");
+        System .out. println (" { .   . }        / Hello  \\");
+        System .out. println (" {   o   }       < Junior   |");
+        System .out. println ("    \\  /          \\ Coder! /");
+        System .out. println ("     ||             -------");
+    }
+}
